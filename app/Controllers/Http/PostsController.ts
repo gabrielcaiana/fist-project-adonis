801d2c31e4973202f3972191cefd0cc9509a17fe
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-05-24 07:44:40 -0300

    configurando o arquivo docker-componse

---
+++ app/Controllers/Http/PostsController.ts
@@ -0,0 +1,7 @@
+// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class PostsController {
+    index() {
+        return ['Post 1', 'Post 2', 'Post 3']
+    }
+}
