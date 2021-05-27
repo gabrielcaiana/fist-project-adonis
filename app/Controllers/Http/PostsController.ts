3564dba571a306ba2d971970874ac4620d4b67bd
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-05-27 19:45:02 -0300

    criando metodo store para criar um post

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,22 +1,28 @@
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Post from 'App/Models/Post'
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import Post from "App/Models/Post";
 
 export default class PostsController {
-  public async index ({}: HttpContextContract) {
-    const posts = await Post.all()
+  public async index({}: HttpContextContract) {
+    const posts = await Post.all();
 
-    return posts
+    return posts;
   }
 
-  public async store ({}: HttpContextContract) {
+  public async store({ request }: HttpContextContract) {
+    // desconstruindo os dados que vem do frontend
+    //const { title, content } = request.all()
+    //const post = await Post.create({title, content})
+
+    const data = await request.only(["title", "content"]);
+
+    const post = await Post.create(data);
+
+    return post;
   }
 
-  public async show ({}: HttpContextContract) {
-  }
+  public async show({}: HttpContextContract) {}
 
-  public async update ({}: HttpContextContract) {
-  }
+  public async update({}: HttpContextContract) {}
 
-  public async destroy ({}: HttpContextContract) {
-  }
+  public async destroy({}: HttpContextContract) {}
 }
