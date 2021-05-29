17e599cd3decd9c104bca505536e5cc18dba9408
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-05-28 23:02:39 -0300

    configurando controller de login

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,10 +1,21 @@
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 
 export default class AuthController {
+  public async store({ request, auth }: HttpContextContract) {
+    // capturo os dados da requisicao 
+    const { email, password } = request.all();
 
-  public async store ({}: HttpContextContract) {
+    // cria uma tentiva de autiticacao com o metodo auth.attempt e declara o tempo para expiracao do token
+    const token = await auth.attempt(email, password, {
+      expiresIn: "1 days",
+    });
+
+    // retorna o token para o frontend
+    return token;
   }
-  
-  public async destroy ({}: HttpContextContract) {
+
+  public async destroy({ auth }: HttpContextContract) {
+    // realiza o logout destruindo o token
+    await auth.logout()
   }
 }
