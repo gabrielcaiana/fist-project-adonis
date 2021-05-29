c819e122117fe150f68444c9a943227b9f19080e
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-05-29 11:39:10 -0300

    criando middleware de Acl para proteger rotas por nivel de permissao

---
+++ app/Middleware/Acl.ts
@@ -0,0 +1,13 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class Acl {
+  public async handle ({ auth, response }: HttpContextContract, next: () => Promise<void>, allowedRoles: string[]) {
+    // code for middleware goes here. ABOVE THE NEXT CALL
+    const user = await auth.authenticate()
+
+    if(!allowedRoles.includes(user.role)) {
+      return response.unauthorized({ error: { messsage: 'Access denied'}})
+    }
+    await next()
+  }
+}
--- start/routes/posts.ts
+++ start/routes/posts.ts
@@ -6,7 +6,7 @@ import Route from '@ioc:Adonis/Core/Route'
 // com o comando no terminal node ace list:routes consigo visualizar todas as rotas criadas
 Route.resource('/posts', 'PostsController').apiOnly().middleware({
     //definindo quais metodos do controller serao protegidos e quais serao as middlewares
-    store: ['auth'],
-    update: ['auth'],
-    destroy: ['auth']
+    store: ['acl:admin'],
+    update: ['acl:admin'],
+    destroy: ['acl:admin']
 })
